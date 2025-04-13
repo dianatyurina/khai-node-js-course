162e5890ce9a21ba77464d373d79dbcfbc02bc8b
dianatyurina <d.r.tyurina@student.khai.edu>
2025-04-13 21:10:03 +0300

    Migrations

---
+++ drizzle.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    schema: './src/db/schema.js',
+    out: './drizzle',
+    driver: 'pg', // <-- Use 'driver', not 'dialect'
+    dbCredentials: {
+      host: '127.0.0.1',
+      port: 5432,
+      user: 'nodejs_course_admin',
+      password: 'my_password',
+      database: 'nodejs_course_database',
+      ssl: false,
+    },
+  };
